e42c08f7406c506c1511c807696528154e5a9218
kinghansmd <hansmdbyhanstztech@gmail.com>
2025-01-07 17:20:19 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,82 @@
+//#ENJOY
+const fs = require("fs-extra");
+if (fs.existsSync(".env"))
+  require("dotenv").config({ path: __dirname + "/.env" });
+global.audio = "";
+global.video = "";
+global.port = process.env.PORT;
+global.appUrl = process.env.APP_URL || "";
+global.email = "hanstzgames@gmail.com";
+global.location = "Dodoma, Tanzania";
+global.mongodb = process.env.MONGODB_URI || "";
+global.allowJids = process.env.ALLOW_JID || "null";
+global.blockJids = process.env.BLOCK_JID || "null";
+global.DATABASE_URL = process.env.DATABASE_URL || "";
+global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Lagos";
+global.github = process.env.GITHUB || "https://github.com/Mrhanstz/MATRIX-MD";
+global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VasiOoR3bbUw5aV4qB31";
+global.website = process.env.GURL || "https://whatsapp.com/channel/0029VasiOoR3bbUw5aV4qB31";
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://files.catbox.moe/perl56.jpeg";
+global.devs = "https://wa.me/255760774888";
+global.sudo = process.env.SUDO || "255760774888";
+global.owner = process.env.OWNER_NUMBER || "255760774888";
+global.style = process.env.STYLE || "3";
+global.gdbye = process.env.GOODBYE || "true";
+global.wlcm = process.env.WELCOME || "true";
+global.warncount = process.env.WARN_COUNT || 3;
+global.disablepm = process.env.DISABLE_PM || "false";
+global.disablegroup = process.env.DISABLE_GROUPS || "false",
+global.MsgsInLog = process.env.MSGS_IN_LOG || "false";
+global.userImages = process.env.USER_IMAGES || "https://files.catbox.moe/perl56.jpeg";
+global.waPresence = process.env.WAPRESENCE || "online";
+global.readcmds = process.env.READ_COMMAND || "true";
+global.readmessage = process.env.READ_MESSAGE || "false";
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
+global.read_status = process.env.AUTO_READ_STATUS || "true";
+global.save_status = process.env.AUTO_SAVE_STATUS || "false";
+global.save_status_from = process.env.SAVE_STATUS_FROM || "255760774888";
+global.read_status_from = process.env.READ_STATUS_FROM || "255760774888";
+
+global.api_smd = "https://api-smd-1.vercel.app";
+global.scan = "https://secret-garden-43998-4daad95d4561.herokuapp.com/";
+
+
+// put your session id down here
+// Don't change otjer things
+// Put your session id inside " " 
+
+global.SESSION_ID =
+  process.env.SESSION_ID ||
+  " "
+module.exports = {
+  menu: process.env.MENU || "3",
+  HANDLERS: process.env.PREFIX || ",",
+  BRANCH: process.env.BRANCH || "main",
+  VERSION: process.env.VERSION || "1.0.0",
+  caption: process.env.CAPTION || "`MATRIX-XMD™`",
+  author: process.env.PACK_AUTHER || "HANSTZ",
+  packname: process.env.PACK_NAME || "MATRIX-XMD BYHANSTZ",
+  botname: process.env.BOT_NAME || "MATRIX-XMD",
+  ownername: process.env.OWNER_NAME || "HANSTZ",
+  errorChat: process.env.ERROR_CHAT || "",
+  KOYEB_API: process.env.KOYEB_API || "false",
+  REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
+  antilink_values: process.env.ANTILINK_VALUES || "all",
+  HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
+  aitts_Voice_Id: process.env.AITTS_ID || "37",
+  ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "public",
+  LANG: (process.env.THEME || "MATRIX-XMD").toUpperCase(),
+};
+global.rank = "updated";
+global.isMongodb = false;
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+  fs.unwatchFile(file);
+  console.log(`Update'${__filename}'`);
+  delete require.cache[file];
+  require(file);
+});
